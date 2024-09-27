cf644a19f22ea663613276de7641ef7092bbfb72
Ivan <at0mskxiv@gmail.com>
2024-09-28 03:57:14 +0400

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python catalogBot.py
---
+++ catalogBot.py
@@ -0,0 +1,35 @@
+import asyncio
+import random
+import requests
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+import nest_asyncio
+import os
+
+token = os.getenv('TELEGRAM_TOKEN')
+
+# Allow nested event loops
+nest_asyncio.apply()
+
+# Replace with your web app URL
+WEB_APP_URL = "https://at0mskxiv.github.io/testCatalog/index.html"  # Update with the correct URL of your Mini App
+
+# Start command to introduce the bot
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    keyboard = [
+        [InlineKeyboardButton("Open Mini App", web_app=dict(url=WEB_APP_URL))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Welcome to the App Catalog! Click the button below to launch the Mini App:', reply_markup=reply_markup)
+
+async def main() -> None:
+    application = ApplicationBuilder().token(token).build()
+
+    # Register commands
+    application.add_handler(CommandHandler('start', start))
+
+    # Start polling and run the bot
+    await application.run_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
